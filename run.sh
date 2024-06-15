8176a7afded0622d38ad75cd569ce364b758c86b
junhyeong <phjpurpleoob@gmail.com>
2024-06-15 17:57:04 +0900

    # Change SH file for usage

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
+read -p "Enter the root path name : " root_path
 read -p "Enter the input video file name : " video_file
 read -p "Enter the input docx file name : " docx_file
 read -p "Enter the name of output wav file name : " wav_file
+
+if [ -z "$root_path" ]; then
+    echo "Error: Root path name is required."
+    exit 1
+fi
 
 if [ -z "$video_file" ]; then
     echo "Error: Video file name is required."
@@ -17,6 +23,6 @@ if [ -z "$wav_file" ]; then
     exit 1
 fi
 
-python init.py "$video_file" "$docx_file" "$wav_file"
-python generate_tts.py "$video_file" "$wav_file"
+python init.py "$root_path" "$video_file" "$docx_file" "$wav_file"
+python generate_tts.py "$video_file" "$wav_file" "$root_path"
 
--- webrun.sh
+++ webrun.sh
@@ -1,5 +1,5 @@
-if [ "$#" -ne 4 ]; then
-    echo "Usage: bash webrun.sh <CONDA_ENV_NAME> <VIDEO_FILE_NAME> <DOCX_FILE_NAME> <WAV_FILE_NAME>"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: bash webrun.sh <CONDA_ENV_NAME> <VIDEO_FILE_NAME> <DOCX_FILE_NAME> <WAV_FILE_NAME> <ROOT_PATH>"
     exit 1
 fi
 
@@ -7,15 +7,18 @@ input1=$1
 input2=$2
 input3=$3
 input4=$4
+input5=$5
+input6=$6
 
 echo "Input1: $input1"
 echo "Input2: $input2"
 echo "Input3: $input3"
 echo "Input4: $input4"
+echo "Input5: $input5"
 
 source ~/anaconda3/etc/profile.d/conda.sh
 
 conda activate "$input1"
 
-python  /home/jun/tomato/init.py "$input2" "$input3" "$input4"
-python  /home/jun/tomato/generate_tts.py "$input2" "$input4"
+python  "$input5"/init.py "$input5" "$input2" "$input3" "$input4"
+python  "$input5"/generate_tts.py "$input2" "$input4" "$input5"
